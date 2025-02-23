b8d8ae741ab2b50c3dcbc0aee23d8452d71e2b5c
minh-dev1801 <minhnv1801@gmail.com>
2025-02-23 22:59:20 +0700

    Init project with TailwinCSS

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./admin/**/*.{html,js}",
+    "./customer/**/*.{html,js}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
